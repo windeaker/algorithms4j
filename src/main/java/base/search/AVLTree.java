2755cecdf04347769d98dd56d0fbc40e81d655d4
windeaker@arch <windeaker@foxmail.com>
2019-11-06 11:15:55 +0800

    push

--- src/main/java/base/search/AVLTree.java
+++ src/main/java/base/search/AVLTree.java
@@ -1,7 +1,155 @@
 package base.search;
 
-public class AVLTree<E extends Comparable> {
-    public class Node<E extends Comparable>{
+import sun.invoke.util.VerifyAccess;
 
+import java.util.List;
+
+/**
+ * 平衡二叉树 AVL
+ * 定义：1.二叉排序树 2.左右子树都是平衡二叉树，左右子树高度差的绝对值之差不超过1
+ * @param <E>
+ */
+public class AVLTree<E extends Comparable> {
+    private AVLNode<E> root;
+    public AVLTree(){
+        root=null;
+    }
+
+    public void buildAVLTreeFromList(List<E> originList){
+
+    }
+
+    public void addValue(E value){
+        AVLNode<E> nNode=new AVLNode<>(value);
+    }
+    public void delValue(){
+
+    }
+
+    public AVLNode<E> getValue(){
+
+    }
+
+    public void setValue(){
+
+    }
+
+    private boolean checkForAdd(){
+
+    }
+
+    private boolean balanceAjust(){
+
+    }
+
+
+    private AVLNode<E> llRotate(AVLNode oldNode){
+        AVLNode nNode=oldNode.left;
+        AVLNode temp=nNode.right;
+        nNode.right=oldNode;
+        oldNode.left=temp;
+
+        oldNode.high=Math.max(oldNode.left.high,oldNode.right.high)+1;
+        nNode.high=Math.max(nNode.left.high,nNode.right.high)+1;
+        return nNode;
+    }
+
+    private AVLNode<E> lrRotate(AVLNode oldNode){
+        oldNode.left=rrRotate(oldNode.left);
+        return llRotate(oldNode);
+    }
+
+    private AVLNode<E> rlRotate(AVLNode oldNode){
+        oldNode.right=llRotate(oldNode.right);
+        return rrRotate(oldNode);
+    }
+
+    private AVLNode<E> rrRotate(AVLNode oldNode){
+        AVLNode nNode=oldNode.right;
+        AVLNode temp=nNode.left;
+        nNode.left=oldNode;
+        oldNode.right=temp;
+
+        oldNode.high=Math.max(oldNode.left.high,oldNode.right.high)+1;
+        nNode.high=Math.max(nNode.left.high,nNode.right.high)+1;
+        return nNode;
+
+    }
+    public class AVLNode<E extends Comparable>{
+        int high;
+        E value;
+        AVLNode<E> left;
+        AVLNode<E> right;
+        AVLNode<E> parent;
+
+        public AVLNode(E value){
+            this.value=value;
+            this.high=0;
+            left=right=parent=null;
+        }
+
+        public int getHigh() {
+            return high;
+        }
+
+        public void setHigh(int high) {
+            this.high = high;
+        }
+
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+        public AVLNode<E> getLeft() {
+            return left;
+        }
+
+        public void setLeft(AVLNode<E> left) {
+            this.left = left;
+        }
+
+        public AVLNode<E> getRight() {
+            return right;
+        }
+
+        public void setRight(AVLNode<E> right) {
+            this.right = right;
+        }
+
+        public AVLNode<E> getParent() {
+            return parent;
+        }
+
+        public void setParent(AVLNode<E> parent) {
+            this.parent = parent;
+        }
+
+        public void highIncre(){
+            high++;
+        }
+
+        public void highDecre(){
+            high--;
+        }
+
+        public int absHigh(AVLNode<E> comapre){
+            return Math.abs(high-comapre.getHigh());
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "high=" + high +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+    public enum AjustType{
+        LL,LR,RR,RL;
     }
 }
--- src/main/java/base/search/BinarySearchTree.java
+++ src/main/java/base/search/BinarySearchTree.java
@@ -1,22 +1,37 @@
 package base.search;
 
+import java.util.List;
+
+/**
+ * BinarySearchTree 二叉搜索排序树（BST）
+ * 定义：
+ * 1. 是个二叉树，或者空树，如果是二叉树，需要满足2,3
+ * 2. 若左子树不为空，则左子树上所有关键字的值均小于根关键字的值。
+ * 3. 若右子树不为空，则右子树上所有关键字的值均小于根关键字的值。
+ *
+ * @param <E>
+ */
 public class BinarySearchTree<E extends Comparable> {
 
-    private BinTreeNode<E> root;
+    private BSTNode<E> root;
 
     public BinarySearchTree() {
         root = null;
     }
 
-    public boolean insert(E data) {
-        BinTreeNode preCursor = null;
-        BinTreeNode cursor = root;
+    public boolean add(E data) {
+        if (data == null) {
+            throw new NullPointerException("数值不能为空");
+        }
+        BSTNode preCursor = null;
+        BSTNode cursor = root;
         int compare = 0;
         while (cursor != null) {
             compare = data.compareTo(cursor.data);
             if (compare == 0) {
                 throw new RuntimeException("数据" + data + "已经存在");
             } else if (compare > 0) {
+                //cursor小于key
                 preCursor = cursor;
                 cursor = cursor.right;
             } else {
@@ -24,7 +39,7 @@ public class BinarySearchTree<E extends Comparable> {
                 cursor = cursor.left;
             }
         }
-        cursor = new BinTreeNode(data);
+        cursor = new BSTNode(data);
         if (compare > 0) {
             preCursor.right = cursor;
         } else if (compare < 0) {
@@ -35,16 +50,123 @@ public class BinarySearchTree<E extends Comparable> {
         return true;
     }
 
-    private class BinTreeNode<E extends Comparable> {
-        E data;
-        BinTreeNode left;
-        BinTreeNode right;
+    public void buildBstFromList(List<E> origins) {
+        if (origins != null && !origins.isEmpty()) {
+            clear();
+            for (E data : origins) {
+                add(data);
+            }
+        }
+    }
 
-        public BinTreeNode(E data) {
+    public E delete(E key) {
+        BSTNode preCursor = null;
+        BSTNode cursor = root;
+        int compare;
+        //cursor是不是父节点的左子树
+        boolean isLeft=true;
+        while (cursor != null) {
+            compare = key.compareTo(cursor.data);
+            if (compare == 0) {
+                break;
+            } else if (compare > 0) {
+                //cursor小于key
+                preCursor = cursor;
+                cursor = cursor.right;
+                isLeft=false;
+            } else {
+                preCursor = cursor;
+                cursor = cursor.left;
+                isLeft=true;
+            }
+        }
+        if (cursor.left == null && cursor.right == null) {
+            simpleDelNode(isLeft,preCursor,null);
+        } else if (cursor.left == null && cursor.right != null) {
+            simpleDelNode(isLeft,preCursor,cursor.right);
+        } else if (cursor.left != null && cursor.right == null) {
+            simpleDelNode(isLeft,preCursor,cursor.left);
+        } else if (cursor.left != null && cursor.right != null) {
+            //找到左子树的最大值节点，和cursor对换，然后删掉最大值节点
+            //或者找到右子树的最小值节点和，cursor对换值，然后删掉最小值节点
+            //这里以左子树为例
+            BSTNode maxPreCursor=cursor;
+            BSTNode maxCursor=maxPreCursor.right;
+            while(maxCursor.right!=null){
+                maxPreCursor=maxCursor;
+                maxCursor=maxPreCursor.right;
+            }
+
+            Comparable temp=cursor.data;
+            cursor.data=maxCursor.data;
+            if (maxCursor.left==null){
+                simpleDelNode(false,maxPreCursor,null);
+            }else{
+                simpleDelNode(false,maxPreCursor,maxCursor.left);
+            }
+            return (E)temp;
+        }
+        return (E)cursor.data;
+    }
+
+    public void simpleDelNode(boolean isLeft,BSTNode preCursor,BSTNode nextNode){
+        if (null==preCursor){
+            root=nextNode;
+        }else {
+            if(isLeft){
+                preCursor.left = nextNode;
+            }else{
+                preCursor.right = nextNode;
+            }
+        }
+    }
+
+    public void set(E old, E nVal) {
+        BSTNode cursor = get(old);
+        if (cursor != null) {
+            cursor.data = nVal;
+        }
+    }
+
+    public BSTNode<E> get(E key) {
+        if (key != null) {
+            BSTNode cursor = root;
+            int compare;
+            while (cursor != null) {
+                compare = cursor.data.compareTo(cursor.data);
+                if (compare == 0) {
+                    return cursor;
+                } else if (compare > 0) {
+                    // cursor 大于key
+                    cursor = cursor.left;
+                } else {
+                    cursor = cursor.right;
+                }
+            }
+            return cursor;
+        } else {
+            return null;
+        }
+    }
+
+    public BSTNode clear() {
+        BSTNode cursor = root;
+        root = null;
+        return cursor;
+    }
+
+
+    private class BSTNode<E extends Comparable> {
+        E data;
+        BSTNode left;
+        BSTNode right;
+
+        public BSTNode(E data) {
             left = null;
             right = null;
             this.data = data;
         }
+
 
         @Override
         public String toString() {
